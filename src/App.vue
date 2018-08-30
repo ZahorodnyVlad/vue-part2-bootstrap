9f44d56a7039185da7e36fe30f8f7fa28fec5227
Vlad Zahorodny <zahorodny.f5@gmail.com>
2018-08-30 09:27:37 +0300

    30.08.18 work

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,72 @@
 <template>
   <div class="container">
     <form class="pt-3">
+
       <div class="form-group">
         <label for="email">Email</label>
         <input
           type="email"
           id="email"
           class="form-control"
-          v-madel="email"
+          :class="{'is-invalid' : $v.email.$error}"
+          @blur="$v.email.$touch()"
+          v-model="email"
         >
+        <div class="invalid-feedback" v-if="!$v.email.required">Field is rquired</div>
+        <div class="invalid-feedback" v-if="!$v.email.email">error</div>
       </div>
+
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          :class="{'is-invalid' : $v.password.$error}"
+          @blur="$v.password.$touch()"
+          v-model="password"
+        >
+        <div class="invalid-feedback" v-if="!$v.password.minLength">min length is {{ $v.password.$params.minLength.min }}. Now it is {{ password.length }}</div>
+      </div>
+
+      <div class="form-group">
+        <label for="confirm">Confirm Password</label>
+        <input
+          type="password"
+          id="confirm"
+          class="form-control"
+          :class="{'is-invalid' : $v.confirmPass.$error}"
+          @blur="$v.confirmPass.$touch()"
+          v-model="confirmPass"
+        >
+        <div class="invalid-feedback" v-if="!$v.confirmPass.sameAs">different password</div>
+      </div>
+
     </form>
   </div>
 </template>
 
   <script>
+    import { required, email, minLength, sameAs } from 'vuelidate/lib/validators'
 
 export default {
   data(){
     return {
-      email: ''
+      email: '',
+      password: '',
+      confirmPass: ''
+    }
+  },
+  validations: {
+    email: {
+      required,
+      email
+    },
+    password: {
+      minLength: minLength(6)
+    },
+    confirmPass: {
+      sameAs: sameAs('password')
     }
   }
 }
